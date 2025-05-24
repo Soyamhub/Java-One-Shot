913d9e4475e8c077d3ff1b66d03079dd6e2e23a2
Soyam Shubham Swain <soyamshubhamswain@gmail.com>
2025-05-24 09:51:07 +0530

    Adding pattern 2

---
+++ Patterns/Pattern2.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Pattern2 {
+    public static void main(String[] args) {
+    // * 
+    // * * 
+    // * * *
+    // * * * *
+    // * * * * *
+    // * * * * * *
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write the number of lines you want");
+        int num = sc.nextInt();
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
